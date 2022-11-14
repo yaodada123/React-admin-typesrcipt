fc465a5bc3aaacf610aaa7136ea75e4ed57f965b
yhc <2580577036@qq.com>
2022-11-14 10:12:36 +0800

    主菜单组件抽离

---
+++ src/components/MainMenu/index.tsx
@@ -0,0 +1,62 @@
+// 主菜单内容抽离
+import {
+    DesktopOutlined,
+    FileOutlined,
+    PieChartOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Layout, Menu } from 'antd';
+import React, { useState } from 'react';
+import {  useNavigate } from 'react-router-dom';
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[],
+): MenuItem {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    } as MenuItem;
+}
+
+const items: MenuItem[] = [
+    getItem('Option 1', '/page1', <PieChartOutlined />),
+    getItem('Option 2', '/page2', <DesktopOutlined />),
+    getItem('User', 'sub1', <UserOutlined />, [
+        getItem('Tom', '3'),
+        getItem('Bill', '4'),
+        getItem('Alex', '5'),
+    ]),
+    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('Files', '9', <FileOutlined />),
+];
+
+const Comp = () => {
+    const rootSubmenuKeys = ['sub1', 'sub2'];
+    const [openKeys, setOpenKeys] = useState(['']);
+    const onOpenChange: MenuProps['onOpenChange'] = keys => {
+        const latestOpenKey = keys.find(key => openKeys.indexOf(key) === -1);
+        if (rootSubmenuKeys.indexOf(latestOpenKey!) === -1) {
+            setOpenKeys(keys);
+        } else {
+            setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+        }
+    };
+    const navigateTo = useNavigate();
+    const menuClick = (e: { key: string }) => {
+        navigateTo(e.key)
+    }
+    return (
+        <Menu theme="dark" defaultSelectedKeys={['/page1']} mode="inline" items={items} onClick={menuClick} openKeys={openKeys}
+                        onOpenChange={onOpenChange} />
+    )
+}
+
+export default Comp;
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,55 +1,18 @@
-import {
-    DesktopOutlined,
-    FileOutlined,
-    PieChartOutlined,
-    TeamOutlined,
-    UserOutlined,
-} from '@ant-design/icons';
-import type { MenuProps } from 'antd';
 import { Breadcrumb, Layout, Menu } from 'antd';
-import React, { useState } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
 const { Header, Content, Footer, Sider } = Layout;
-type MenuItem = Required<MenuProps>['items'][number];
+import MainMenu from "src/components/MainMenu/index"
 
-function getItem(
-    label: React.ReactNode,
-    key: React.Key,
-    icon?: React.ReactNode,
-    children?: MenuItem[],
-): MenuItem {
-    return {
-        key,
-        icon,
-        children,
-        label,
-    } as MenuItem;
-}
-
-const items: MenuItem[] = [
-    getItem('Option 1', '/page1', <PieChartOutlined />),
-    getItem('Option 2', '/page2', <DesktopOutlined />),
-    getItem('User', 'sub1', <UserOutlined />, [
-        getItem('Tom', '3'),
-        getItem('Bill', '4'),
-        getItem('Alex', '5'),
-    ]),
-    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-    getItem('Files', '9', <FileOutlined />),
-];
 const Home = () => {
     const [collapsed, setCollapsed] = useState(false);
-    const navigateTo =  useNavigate();
-    const menuClick = (e: {key: string}) => {
-        navigateTo(e.key)
-    }
     return (
         <div>
             <Layout style={{ minHeight: '100vh' }}>
                 {/* 侧边栏 */}
                 <Sider collapsible collapsed={collapsed} onCollapse={value => setCollapsed(value)}>
                     <div className="logo" />
-                    <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} onClick={menuClick} />
+                    <MainMenu></MainMenu>
                 </Sider>
                 {/* 右侧区域 */}
                 <Layout className="site-layout">
@@ -67,7 +30,7 @@ const Home = () => {
                     {/* 页尾内容 */}
                     <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
                 </Layout>
-                
+
             </Layout>
         </div>
     )
